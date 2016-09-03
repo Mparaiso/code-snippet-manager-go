b311c326494ee3195894f0ebf9d2728b0050a5af
mparaiso <aikah@free.fr>
2016-09-03 18:52:11 +0200

    template inheritance

--- main.py
+++ main.py
@@ -11,23 +11,32 @@ import flask
 
 app = flask.Flask(__name__)
 
-# homepage
 @app.route('/')
 def index():
+    """ exemple of use of request object """
     user_agent = flask.request.headers.get('User-Agent')
     return "Hello Code Snippet Manager \n your user agent is %s " % user_agent
 
-# returning a tuple body,status,headers
 @app.route("/badrequest")
-def basrequest():
+def badrequest():
+    """ exemple of returning a tuple from an handler """
     return "Bad Request",400,{"X-HTTP-Special-Header":"Some special header"}
 
+@app.route("/makeresponse")
+def makeresponse():
+    """ creating a response object with make_response """    
+    response = flask.make_response("Some response")
+    response.set_cookie("answer",'2')
+    return response
 
+@app.route('/redirect')
+def redirect():
+    return flask.redirect("http://yahoo.com")
 
-# route with variable
 @app.route("/user/<name>")
 def user(name):
-    return "Hello %s" % name
+    """example of route with variable rendered in a template """
+    return flask.render_template("user.j2.html",name=name)
 
 # entry point
 if __name__ == '__main__':
